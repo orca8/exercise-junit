492951609c85f8f0ccb074b8b0a43a49368cbf34
orca8 <orca8@users.noreply.github.com>
2014-03-23 01:42:00 +0900

    Add testcase method return type void
    戻り値がvoid型であるメソッドは戻り値による検証ができないので
    状態の変化を計測するためのメソッドやフィールドを参照することで検証を行います。
    
    計測する方法を今回はメソッドで行っているが、計測で利用したメソッドも利用される
    メソッドを使用しなければ十分な検証を行うことができない。
    いくつかのメソッドが相互作用するクラスは、テストでも複雑になる。
    
    いくつかのメソッドのテストが相互依存している場合、テストの成否が他のテストの結果に
    影響し、テスト全体の信頼性を保つことが困難になる。
    今回のコードであればgetNumメソッドのテストが失敗していると、関連するテストはすべて
    期待された動きをしていない可能性がある。

---
+++ tests/orca8/ItemStockTest.java
@@ -0,0 +1,69 @@
+package orca8;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * exercise-junit
+ */
+@RunWith(Enclosed.class)
+public class ItemStockTest {
+    public static class 初期状態の場合 {
+        ItemStock sut;
+        Item book;
+
+        @Before
+        public void setUp() throws Exception {
+            book = new Item("book", 3000);
+            sut = new ItemStock();
+        }
+
+        @Test
+        public void getNumはbookで0を返す() throws Exception {
+            assertThat(sut.getNum(book), is(0));
+        }
+
+        @Test
+        public void addでbookを追加するとgetNumで1を返す() throws Exception {
+            sut.add(book);
+            assertThat(sut.getNum(book), is(1));
+        }
+    }
+
+    public static class bookが1回追加されている場合 {
+
+        ItemStock sut;
+        Item book;
+
+        @Before
+        public void setUp() throws Exception {
+            book = new Item("book", 3000);
+            sut = new ItemStock();
+            sut.add(book);
+        }
+
+        @Test
+        public void getNumはbookで1を返す() throws Exception {
+            assertThat(sut.getNum(book), is(1));
+        }
+
+        @Test
+        public void addでbookを追加するとgetNumで2を返す() throws Exception {
+            sut.add(book);
+            assertThat(sut.getNum(book), is(2));
+        }
+
+        @Test
+        public void addでbikeを追加するとgetNumでbookとbikeは1を返す() throws Exception {
+            Item bike = new Item("bike", 50000);
+            sut.add(bike);
+            assertThat(sut.getNum(book), is(1));
+            assertThat(sut.getNum(bike), is(1));
+        }
+    }
+}
