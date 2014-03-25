d6beb029b38f672bdde508b557f51232e71a55a3
orca8 <orca8@users.noreply.github.com>
2014-03-26 01:00:28 +0900

    (refs #10)Add assertion case number

--- tests/orca8/FizzBuzzTest.java
+++ tests/orca8/FizzBuzzTest.java
@@ -17,12 +17,20 @@ public class FizzBuzzTest {
         List<String> actual = FizzBuzz.createFizzBuzzList(16);
         assertThat(actual.size(), is(16));
         assertThat(actual.get(0), is("1"));
+        assertThat(actual.get(1), is("2"));
         assertThat(actual.get(2), is("Fizz"));
+        assertThat(actual.get(3), is("4"));
         assertThat(actual.get(4), is("Buzz"));
         assertThat(actual.get(5), is("Fizz"));
+        assertThat(actual.get(6), is("7"));
+        assertThat(actual.get(7), is("8"));
         assertThat(actual.get(8), is("Fizz"));
         assertThat(actual.get(9), is("Buzz"));
+        assertThat(actual.get(10), is("11"));
         assertThat(actual.get(11), is("Fizz"));
+        assertThat(actual.get(12), is("13"));
+        assertThat(actual.get(13), is("14"));
         assertThat(actual.get(14), is("FizzBuzz"));
+        assertThat(actual.get(15), is("16"));
     }
 }
